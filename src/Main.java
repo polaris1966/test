9b9b323caa038010da72201369badc7b01ad379b
laz1966 <39864011+laz1966@users.noreply.github.com>
2023-04-12 11:35:04 +0500

    SP-6 Создать консольный проект с командами https://pantera-devel.atlassian.net/browse/SP-6 переписал class main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        while (true) {
             if (args[0].equals("echo")) {
                 System.out.println(args[1]);
-                break;
             } else if (args[0].equals("add")) {
                 Integer i1 = Integer.valueOf(args[1]);
                 Integer i2 = Integer.valueOf(args[2]);
                 System.out.println(i1 + i2);
-                break;
             }
-        }
     }
 }
 
